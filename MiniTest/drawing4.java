dea2f333e7110dad163657db591d69ee1fcb4d0f
Alexandru <triboi.alexandru@gmail.com>
2019-08-07 18:51:33 +0300

    add exercises 2.12 and Minitest

---
+++ MiniTest/drawing4.java
@@ -0,0 +1,16 @@
+public class drawing4 {
+
+    public static void main (String...arg) {
+
+        System.out.println("   \\/  ");
+        System.out.println("  \\\\//  ");
+        System.out.println(" \\\\\\///  ");
+        System.out.println(" ///\\\\\\  ");
+        System.out.println("  //\\\\  ");
+        System.out.println("   /\\  ");
+
+
+
+    }
+
+}
